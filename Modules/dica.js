d847143de9a75fa8d3503ca263330fa7f5127319
Tássyo Santana <tassyo.santana@hotmail.com>
2017-10-29 15:11:16 -0200

    Terminado 'Modules' e pausa em 'Directives - Restrictions'

---
+++ Modules/dica.js
@@ -0,0 +1,7 @@
+/*
+
+
+Embora seja comum em aplicativos HTML colocar scripts no final do elemento <body>,
+ é recomendável que você carregue a biblioteca AngularJS na <head> ou no início do <body>.
+
+Isso ocorre porque as chamadas para angular.module só podem ser compiladas após a carga da biblioteca. */
---
+++ Modules/modules_controllers_in_files/myControl.js
@@ -0,0 +1,4 @@
+app.controller("myControl",function($scope){
+    $scope.firstName = "John";
+    $scope.lastName = "Doe";
+});
