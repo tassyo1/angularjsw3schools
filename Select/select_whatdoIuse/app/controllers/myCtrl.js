d242b18ffccb2d15d99238bc4f26df52ae391e3f
Tássyo Santana <tassyo.santana@hotmail.com>
2018-01-29 13:07:23 -0200

    Terminado 'Select'

---
+++ Select/select_whatdoIuse/app/controllers/myCtrl.js
@@ -0,0 +1,7 @@
+app.controller('myCtrl',function($scope){
+    $scope.cars = [
+        {model:"Ford Mustang", color : 'red'},
+        {model:"Fiat 500", color: 'white'},
+        {model:"Volvo XC90", color:'black'}
+    ];
+});
