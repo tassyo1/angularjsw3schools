77aedf46911e6c27b66fa14b9f0e4698f539d5c5
Tássyo Santana <tassyo.santana@hotmail.com>
2018-01-31 17:37:19 -0200

    Terminado 'SQL'

---
+++ SQL/APIAngulaJSW3Schools/Data/Context.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APIAngulaJSW3Schools.Models;
+
+namespace APIAngulaJSW3Schools.Data
+{
+    public class Context : DbContext
+    {
+
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>(
+                entity =>
+                {
+                    entity.ToTable("Customers");
+                    entity.Property(e => e.Id).HasColumnName("Id");
+                    entity.Property(e => e.Name).HasColumnName("Name");
+                    entity.Property(e => e.City).HasColumnName("City");
+                    entity.Property(e => e.Country).HasColumnName("Country");
+
+
+                });
+
+        }
+    }
+}
---
+++ SQL/APIAngulaJSW3Schools/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using APIAngulaJSW3Schools.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIAngulaJSW3Schools
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddDbContext<Context>(options => options.UseSqlServer(Configuration.GetConnectionString("Casa")));
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+
+            //Controle de Acesso HTTP - permitindo que o dominio 'localhost:8080' faça requisição na API
+            app.UseCors( options => options.WithOrigins("http://localhost:8080").AllowAnyMethod());
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
